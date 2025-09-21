e71353ad9784d4480547c4027ce6a94c9ff3a2fe
Jignesh Atul Paratane <jigneshparatane@gmail.com>
2025-09-21 18:33:01 +0530

    modified the connection.go file to integrate the pgxpool library and added comments to the Connect function and marked for deprecation in subsequent commits

--- database/connection.go
+++ database/connection.go
@@ -2,10 +2,13 @@ package database
 
 import (
 	"context"
-	"log"
-	 "os"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
@@ -16,6 +19,25 @@ func init() {
 	}
 }
 
+func generateConnString() (string, error) {
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_URL_OR_IP")
+    port := os.Getenv("DB_PORT")
+    dbname := os.Getenv("DB_NAME")
+    
+    if user == "" || password == "" || host == "" || port == "" || dbname == "" {
+        return "", fmt.Errorf("required environment variables not set")
+    }
+    
+    connectionURL := fmt.Sprintf(
+        "postgres://%s:%s@%s:%s/%s",
+        user, password, host, port, dbname)
+    
+    return connectionURL, nil
+}
+
+//Need to Deprecate use of *pgx.Conn for Concurrency handling
 func Connect() *pgx.Conn {
 	connection:=os.Getenv("CONNECTION_URL")
 	conn, err := pgx.Connect(context.Background(), connection)
@@ -23,6 +45,35 @@ func Connect() *pgx.Conn {
 		log.Fatal("Failed to connect to the database:", err)
 	}
 	return conn
+}
+
+func PoolConnect() *pgxpool.Pool {
+	connectionURL, err:=generateConnString()
+	if err!=nil{
+		log.Fatalf("Database configuration error %s", err)
+	}
+
+	ctx, cancel:=context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err:=pgxpool.ParseConfig(connectionURL)
+	if err!=nil{
+		log.Fatalf("Failed to parse DB config %s", err)
+	}
+
+	config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		_, err:=conn.Exec(ctx, 
+		`SET TIME ZONE 'UTC';
+		SET search_path TO public;`)
+		return err
+	}
+
+	pool,err:=pgxpool.NewWithConfig(ctx, config)
+	if err!=nil {
+		log.Fatalf("Failed to create pool:%s",err)
+	}
+
+	return pool
 }
 
 func CreateUsersTable(conn *pgx.Conn) error {
